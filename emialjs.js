cea9113eb8b6e1ae11173a3b8431a4707d158db0
Naisam37 <naisamta123@gmail.com>
2023-03-19 20:54:47 +0530

    portfolio

---
+++ emialjs.js
@@ -0,0 +1,25 @@
+function sendMail(e){
+    event.preventDefault();
+    console.log(document.getElementById('name').value)
+    console.log(document.getElementById('email').value)
+    console.log(document.getElementById('msg').value)
+    console.log(document.getElementById('subject').value)
+    let loading=document.querySelector('.loading');
+
+
+
+var params={
+        
+    from_name:document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    msg: document.getElementById('msg').value,
+    subject:document.getElementById('subject').value
+    
+    
+}
+emailjs.send("service_g0hukgj","template_rrtm00l",params).then(function(res){
+alert("sucsess"+res.status)})
+
+
+
+}
